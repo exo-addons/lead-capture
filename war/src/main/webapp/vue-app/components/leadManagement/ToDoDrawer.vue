76a48bf73aeae033961f13d1e72b34012e9a7837
mkrout <mkrout@exoplatform.com>
2020-03-09 14:22:24 +0100

    Add ability to add personal tasks

---
+++ war/src/main/webapp/vue-app/components/leadManagement/ToDoDrawer.vue
@@ -0,0 +1,174 @@
+<template>
+<v-container class="">
+    <v-list-item>
+        <v-list-item-title>
+            Todo list
+        </v-list-item-title>
+        <v-switch v-model="showAll" label="All"></v-switch>
+        <v-btn class="ma-2" outlined @click="addNote()">
+            <v-icon>mdi-plus</v-icon>
+            Add Item
+        </v-btn>
+        <a fab dark small style="float: right;" href="/portal/intranet/tasks" target="_blank">
+            <v-icon>mdi-open-in-new</v-icon>
+        </a>
+
+        <v-list-item-action>
+            <i class="uiCloseIcon" @click="toggleDrawer()"></i>
+        </v-list-item-action>
+    </v-list-item>
+    <v-card v-show="dialog">
+        <v-menu ref="datePicker" v-model="datePicker" :close-on-content-click="false" :return-value.sync="date" transition="scale-transition" offset-y min-width="290px">
+            <template v-slot:activator="{ on }">
+                <v-text-field v-model="date" label="Due Date" prepend-icon="event" readonly v-on="on"></v-text-field>
+            </template>
+            <v-date-picker v-model="date" no-title scrollable>
+                <v-spacer></v-spacer>
+                <v-btn text color="primary" @click="datePicker = false">Cancel</v-btn>
+                <v-btn text color="primary" @click="$refs.datePicker.save(date)">OK</v-btn>
+            </v-date-picker>
+        </v-menu>
+        <v-card-text>
+            <v-text-field v-model="title" label="Title"></v-text-field>
+        </v-card-text>
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn @click="close()" color="blue darken-1" text>Close</v-btn>
+            <v-btn @click="saveTask()" color="blue darken-1" text>Save</v-btn>
+        </v-card-actions>
+    </v-card>
+    <v-list>
+        <v-list-item :key="item.title" v-for="item in tasksList" class="taskItem">
+            <v-list-item-action>
+                <v-btn icon>
+                    <v-icon v-if="item.completed" color="green lighten-1" large>mdi-check-circle</v-icon>
+                    <v-icon v-else color="grey lighten-1" large @click="completeTask(item)">mdi-check-circle</v-icon>
+                </v-btn>
+
+            </v-list-item-action>
+            <v-list-item-content>
+                <v-list-item-title v-text="item.title"></v-list-item-title>
+                <v-list-item-subtitle v-text="item.formattedDueDate"></v-list-item-subtitle>
+            </v-list-item-content>
+        </v-list-item>
+    </v-list>
+</v-container>
+</template>
+
+<script>
+export default {
+    props: ['lead', 'tasks'],
+    data: () => ({
+        date: new Date().toISOString().substr(0, 10),
+        dialog: false,
+        title: "",
+        datePicker: false,
+        newItem: {},
+        showAll: false,
+
+    }),
+    computed: {
+        tasksList() {
+            if (this.showAll) {
+                return this.tasks
+            }
+            return this.tasks.filter(item => {
+                return item.completed !== true
+            })
+
+        }
+    },
+    watch: {
+        showAll: function (val) {
+            if (!val) {
+                this.tasksList = this.tasks.filter(item => {
+                    return item.completed === true
+                })
+            } else {
+                this.tasksList = this.tasks
+            }
+        },
+    },
+    methods: {
+
+        toggleDrawer() {
+            this.$emit('toggleToDoDrawer');
+        },
+        addNote() {
+            this.dialog = true
+        },
+        close() {
+            this.newNote = {}
+            this.dialog = false
+        },
+        saveTask() {
+            this.newItem.lead = this.lead
+            this.newItem.formattedDueDate = this.date
+            this.newItem.title = this.title
+
+            fetch(`/portal/rest/leadcapture/leadsmanagement/task`, {
+                    method: 'post',
+                    credentials: 'include',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(this.newItem),
+                })
+                .then((resp) => resp.json())
+                .then((resp) => {
+                    this.tasks.unshift(resp);
+                });
+            this.title = ""
+            this.dialog = false
+
+        },
+        completeTask(item) {
+            item.completed = true
+
+            fetch(`/portal/rest/leadcapture/leadsmanagement/task/` + item.id, {
+                    method: 'put',
+                    credentials: 'include',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(item),
+                })
+                .then((resp) => resp.json())
+                .then((resp) => {
+                    //this.comments.unshift(resp);
+                });
+        }
+    }
+}
+</script>
+
+<style>
+.taskItem {
+    background-color: #f5f5f5;
+    margin-bottom: 5px;
+}
+
+.commentItem {
+    width: 100%;
+}
+
+.avatarXSmall {
+    max-height: 36px;
+    max-width: 36px;
+}
+
+.commmentLeft+.commentRight {
+    margin-left: 50px;
+}
+
+.date {
+    margin-left: 10px;
+    font-size: 12px;
+    padding-right: 16px;
+    float: right;
+}
+
+.comment {
+    width: 100%;
+}
+</style>
