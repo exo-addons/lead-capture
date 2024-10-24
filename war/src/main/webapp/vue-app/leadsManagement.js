16b062bd18f08ecc8001d1a256f0adb386b7b946
Romain Dénarié <romain.denarie@exoplatform.com>
2024-10-24 14:43:05 +0200

    fix : on firefox, the application is not loaded
    Before this fix, the application is not load because the event readyState change arrive sooner, and the dom is not ready
    This commit add a timeout in case the target element id is not present to load the application

--- war/src/main/webapp/vue-app/leadsManagement.js
+++ war/src/main/webapp/vue-app/leadsManagement.js
@@ -11,22 +11,27 @@ const vuetify = new Vuetify({
 });
 
 function initLeadsManagement() {
-  const lang = eXo && eXo.env && eXo.env.portal && eXo.env.portal.language;
-  const urls = [`${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.addon.LeadCapture-${lang}.json`,
-  `${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.portlet.taskManagement-${lang}.json`];
+  if (document.getElementById("leadsManagementApp")) {
+    const lang = eXo && eXo.env && eXo.env.portal && eXo.env.portal.language;
+    const urls = [`${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.addon.LeadCapture-${lang}.json`,
+    `${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.portlet.taskManagement-${lang}.json`];
 
-    exoi18n.loadLanguageAsync(lang, urls).then(i18n => {
-      const vueApp = new Vue({
-          render: (h) => h(leadsManagementApp),
-          i18n,
-          vuetify
-      }).$mount('#leadsManagementApp');
-      Vue.prototype.$vueT = Vue.prototype.$t;
-      Vue.prototype.$t = (key, defaultValue) => {
-          const translation = vueApp.$vueT(key);
-          return translation !== key && translation || defaultValue;
-      }
-  });
+      exoi18n.loadLanguageAsync(lang, urls).then(i18n => {
+        console.log();
+        const vueApp = new Vue({
+            render: (h) => h(leadsManagementApp),
+            i18n,
+            vuetify
+        }).$mount('#leadsManagementApp');
+        Vue.prototype.$vueT = Vue.prototype.$t;
+        Vue.prototype.$t = (key, defaultValue) => {
+            const translation = vueApp.$vueT(key);
+            return translation !== key && translation || defaultValue;
+        }
+    });
+  } else {
+    setTimeout(initLeadsManagement, 100);
+  }
 }
 
 document.onreadystatechange = () => {
