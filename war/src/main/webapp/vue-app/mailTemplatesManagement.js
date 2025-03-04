3a4b56a5623b6aa71d67ebdb78830a38d25fed8e
Mohamed Amine Krout <mkrout@exoplatform.com>
2025-03-04 13:23:20 +0100

    fix: Lead capture administration page are ko - EXO-77227
    Prior to this, Some pages of lead capture administration are empty on tribe
    
    This commit change the initialization of the app to fix the problem.

--- war/src/main/webapp/vue-app/mailTemplatesManagement.js
+++ war/src/main/webapp/vue-app/mailTemplatesManagement.js
@@ -9,23 +9,18 @@ const vuetify = new Vuetify({
   iconfont: 'mdi',
 });
 
+const lang = eXo && eXo.env && eXo.env.portal && eXo.env.portal.language;
+const url = `${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.addon.LeadCapture-${lang}.json`;
 
-document.onreadystatechange = () => {
-  if (document.readyState === "complete") {
-  const lang = eXo && eXo.env && eXo.env.portal && eXo.env.portal.language;
-  const url = `${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.addon.LeadCapture-${lang}.json`;
-
-  exoi18n.loadLanguageAsync(lang, url).then(i18n => {
-      const vueApp = new Vue({
-          render: (h) => h(mailTemplatesManagementApp),
-          i18n,
-          vuetify
-      }).$mount('#mailTemplatesManagementApp');
-      Vue.prototype.$vueT = Vue.prototype.$t;
-      Vue.prototype.$t = (key, defaultValue) => {
-          const translation = vueApp.$vueT(key);
-          return translation !== key && translation || defaultValue;
-      }
-  });
+exoi18n.loadLanguageAsync(lang, url).then(i18n => {
+  const vueApp = new Vue({
+      render: (h) => h(mailTemplatesManagementApp),
+      i18n,
+      vuetify
+  }).$mount('#mailTemplatesManagementApp');
+  Vue.prototype.$vueT = Vue.prototype.$t;
+  Vue.prototype.$t = (key, defaultValue) => {
+      const translation = vueApp.$vueT(key);
+      return translation !== key && translation || defaultValue;
   }
-};
+});
